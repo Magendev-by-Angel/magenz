e96f395071167f30d86d586cedbf2b3402714059
Angel Zúñiga Navarro <angel.zuna@icloud.com>
2022-12-29 19:11:38 +0000

    Scripts to install react, next, chakra and ts

--- index.js
+++ index.js
@@ -1,54 +1,60 @@
 const { execSync } = require("child_process");
 const {
-  theme,
-  navBar,
-  toggleButton,
-  _app,
-  _document,
-  notFound,
-  index,
-  eslint,
-  nextConf,
-  prettier,
-  tsconfig,
-  package,
-  mainLayout,
-  linkItem,
+	theme,
+	navBar,
+	toggleButton,
+	_app,
+	_document,
+	notFound,
+	index,
+	eslint,
+	nextConf,
+	prettier,
+	tsconfig,
+	package,
+	mainLayout,
+	linkItem,
 } = require("./contents.js");
 const fs = require("fs");
 
-const createApp = (appName) => {
-  // fs.writeFileSync("", ``);
-  fs.mkdirSync(appName);
-  process.chdir(appName);
-  fs.mkdirSync("components");
-  fs.mkdirSync("components/layouts");
-  fs.mkdirSync("lib");
-  fs.mkdirSync("pages");
-  fs.mkdirSync("pages/api");
+const createApp = appName => {
+	// fs.writeFileSync("", ``);
+	fs.mkdirSync(appName);
+	process.chdir(appName);
+	fs.mkdirSync("components");
+	fs.mkdirSync("components/layouts");
+	fs.mkdirSync("lib");
+	fs.mkdirSync("pages");
+	fs.mkdirSync("pages/api");
 
-  fs.writeFileSync("components/navBar.tsx", navBar);
-  fs.writeFileSync("components/ThemeToggleButton.tsx", toggleButton);
-  fs.writeFileSync("components/layouts/main.tsx", mainLayout);
-  fs.writeFileSync("components/LinkItem.tsx", linkItem);
-  fs.writeFileSync("lib/theme.ts", theme);
-  fs.writeFileSync("pages/api/index.ts", ``);
-  fs.writeFileSync("pages/404.tsx", notFound);
-  fs.writeFileSync("pages/_app.tsx", _app);
-  fs.writeFileSync("pages/_document.tsx", _document);
-  fs.writeFileSync("pages/index.tsx", index);
+	fs.writeFileSync("components/navBar.tsx", navBar);
+	fs.writeFileSync("components/ThemeToggleButton.tsx", toggleButton);
+	fs.writeFileSync("components/layouts/main.tsx", mainLayout);
+	fs.writeFileSync("components/LinkItem.tsx", linkItem);
+	fs.writeFileSync("lib/theme.ts", theme);
+	fs.writeFileSync("pages/api/index.ts", ``);
+	fs.writeFileSync("pages/404.tsx", notFound);
+	fs.writeFileSync("pages/_app.tsx", _app);
+	fs.writeFileSync("pages/_document.tsx", _document);
+	fs.writeFileSync("pages/index.tsx", index);
 
-  fs.writeFileSync(".eslintrc.json", eslint);
-  fs.writeFileSync(".prettierrc.json", prettier);
-  fs.writeFileSync("next.config.js", nextConf);
-  fs.writeFileSync("tsconfig.json", tsconfig);
-  fs.writeFileSync("package.json", package(appName));
-  fs.writeFileSync(".gitignore", "node_modules");
+	fs.writeFileSync(".eslintrc.json", eslint);
+	fs.writeFileSync(".prettierrc.json", prettier);
+	fs.writeFileSync("next.config.js", nextConf);
+	fs.writeFileSync("tsconfig.json", tsconfig);
+	fs.writeFileSync("package.json", package(appName));
+	fs.writeFileSync(".gitignore", "node_modules");
+	execSync(
+		"npm install react react-dom next @chakra-ui/react @emotion/react @emotion/styled framer-motion"
+	);
+	execSync(
+		"npm install -D eslint prettier typescript @types/node @types/react"
+	);
 };
 
 if (process.argv.length > 2) {
-  const appName = process.argv[2];
-  createApp(appName);
+	const appName = process.argv[2];
+	createApp(appName);
 } else {
-  console.log("Please give a name for your propject");
+	console.log("Please give a name for your propject");
 }
